44998e9231edb9f9016d8d4ea0a9caf685b826a6
Sanket Kulkarni <sanketpkulkarni141@gmail.com>
2021-10-09 20:17:41 +0530

    Configure debugger

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,9 @@
-import { Injectable } from "@nestjs/common";
+import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
   getHello(): string {
-    return "Hello World!22244";
+    console.log('Hello');
+    return 'Hello World';
   }
 }
